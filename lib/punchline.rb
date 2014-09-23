0dfb22b7ef648c4fcbe6d04e2f71ae68092503b5
Chris Atkins <christopherlionelatkins@gmail.com>
2014-09-23 14:31:35 +1000

    Added Punchline#configure which takes a block to configure gem in initializers

--- lib/punchline.rb
+++ lib/punchline.rb
@@ -4,6 +4,10 @@ require 'punchline/min_queue'
 
 module Punchline
   class << self
+    def configure(&block)
+      yield config if block_given?
+    end
+
     def config
       @config ||= Configuration.new
     end
